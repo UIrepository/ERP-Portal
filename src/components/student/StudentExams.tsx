011a7465e00271550c66a1c343251b3cb6208ff8
UIofficialwebsite <uiwebsite638@gmail.com>
2025-07-29 22:29:04 +0530

    Update StudentExams.tsx

--- src/components/student/StudentExams.tsx
+++ src/components/student/StudentExams.tsx
@@ -1,4 +1,3 @@
-import { Card, CardContent } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Trophy, ClipboardCheck, BarChart, FileText } from 'lucide-react';
 
