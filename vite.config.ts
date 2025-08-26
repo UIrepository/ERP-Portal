db8eed1de105d715a44ef5ac6695447ebe467723
UIrepository <distribution.premiumbooks@gmail.com>
2025-08-26 20:20:40 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/", // Explicitly set the base path
   server: {
     host: "::",
     port: 8080,
