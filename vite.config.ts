b0565b4912a2749687970208791194450f0ecb72
UIrepository <distribution.premiumbooks@gmail.com>
2025-08-26 20:52:03 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: "/", // Explicitly set the base path
+export default defineConfig({
+  base: "/", // This ensures all assets are served from the root
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-  ].filter(Boolean),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
