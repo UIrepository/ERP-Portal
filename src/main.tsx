3471ffb21fa7807edb069a26ee171fe63d869070
ROSHAN KUMAR SINGH <businessmarketing0002@gmail.com>
2026-02-08 03:46:09 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import { createRoot } from 'react-dom/client'
 import { inject } from '@vercel/analytics'
 import { SpeedInsights } from '@vercel/speed-insights/react'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import App from './App.tsx'
 import './index.css'
 
 inject()
 
 createRoot(document.getElementById("root")!).render(
-  <>
+  <GoogleOAuthProvider clientId="561606523690-f4a0387bv89guvm5922v725gdtinch1n.apps.googleusercontent.com">
     <App />
     <SpeedInsights />
-  </>
+  </GoogleOAuthProvider>
 );
