d910cff3058ac524c1efc63057683e88074377bd
Tejash Mishra <mtejash07@gmail.com>
2026-03-09 00:27:28 +0530

    security: fix CORS origin to ssp.unknowniitians.com

--- supabase/functions/create-youtube-stream/index.ts
+++ supabase/functions/create-youtube-stream/index.ts
@@ -1,7 +1,7 @@
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 
 const corsHeaders = {
-  'Access-Control-Allow-Origin': 'https://erp-portal-lyart.vercel.app',
+  'Access-Control-Allow-Origin': 'https://ssp.unknowniitians.com',
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
 };
 
